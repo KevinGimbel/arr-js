372ba1f8f76e2b7ecea932e4a6121217753ac498
Christian Fei <crifei93@gmail.com>
2014-12-10 19:55:27 +0100

    added tests for Array.first

--- test/spec/arr.js
+++ test/spec/arr.js
@@ -1,6 +1,17 @@
 describe("arr-js", function() {
 
-  it("should fail", function() {
-    expect(false).toBeTruthy();
+  var simpleArray = [0,1,2,3];
+
+  describe("first", function() {
+    it("should return the first element if no or invalid number is passed", function() {
+      expect(simpleArray.first()).toEqual(0);
+      expect(simpleArray.first('invalid')).toEqual(0);
+      // expect(simpleArray.first(-1)).toEqual(0); //TODO
+      // expect(simpleArray.first(simpleArray.length + 10)).toEqual(0); //TODO
+    });
+
+    it("should return the first n elements if a valid number is passed", function() {
+      expect(simpleArray.first(2)).toEqual([0,1]);
+    });
   });
 });
