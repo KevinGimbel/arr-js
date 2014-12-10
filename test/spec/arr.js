6ab523b11deab0206f20d4f4fb7a25807615988f
Christian Fei <crifei93@gmail.com>
2014-12-10 19:57:40 +0100

    fixed test for invalid number for Array.first

--- test/spec/arr.js
+++ test/spec/arr.js
@@ -6,8 +6,8 @@ describe("arr-js", function() {
     it("should return the first element if no or invalid number is passed", function() {
       expect(simpleArray.first()).toEqual(0);
       expect(simpleArray.first('invalid')).toEqual(0);
-      // expect(simpleArray.first(-1)).toEqual(0); //TODO
-      // expect(simpleArray.first(simpleArray.length + 10)).toEqual(0); //TODO
+      expect(simpleArray.first(-1)).toEqual(0); //TODO
+      // expect(simpleArray.first(simpleArray.length)).toEqual(0); //TODO
     });
 
     it("should return the first n elements if a valid number is passed", function() {
