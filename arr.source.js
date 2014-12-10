230188bf8fc4e4c54efe7759c8d38e383e05db41
kevingimbel <hallo@kevingimbel.com>
2014-12-10 13:37:43 +0100

    v0.0.2, horray! New functions and new docs. \o/

--- arr.source.js
+++ arr.source.js
@@ -5,75 +5,40 @@
  * Arr.js enhances the JavaScript Array Object by providing a set of custom functions. 
  *
  * @author Kevin Gimbel <kevingimbel.com>
- * @version 0.0.1
- * @project page kevingimbel.com/arr-js
+ * @version 0.0.2
+ * @page kevingimbel.com/arr-js
  * 
  *
  *
 */
-"use strict";
 
 
 
 
-
-
-
-
-
-//
-// Array.last()
-//
-// Simpy returns the last element of an array. If a number is given
-// as a parameter it will return the last N elements.
-//
-// @params none, number
-// @return last element or range (when given a number)
-// 
-Array.prototype.last = function(num) {
-  var i;
-  // if there's no number given just return the last element
- if(typeof num !== 'number') {
-   return this[this.length - 1];
- } 
-  // "else", return an array of last entries based on the number provided
-  var dump = [], // a dump array to return
-      array = this; // reassigning this (current array) to a var
-  for(i = 1; i <= num; i++) {
-    dump.push(array[array.length - i]);
-  }
-  return dump; // return the dump array
-}
-
-
-
-
-
-
-
-
-
-//
-// Array.first()
-//
-// Simpy returns the first element of an array. If a number is given
-// as a parameter it will return the first N elements.
-//
-// @params none, number
-// @return first element or range (when given a number)
-// 
+/*
+ * ## Array.first()
+ *
+ * Simply returns the first element of an array. If a number is given
+ * as a parameter it will return the first N elements.
+ * 
+ * ```js
+ * [0,1,2,3].first() // => 0
+ * [0,1,2,3].first(2) // => [0,1]
+ * ```
+ *
+ * @params none, number
+ * @return first element; {array} range
+*/ 
 Array.prototype.first = function(num) {
-  var i;
-
   if(typeof num !== 'number') {
     return this[0];
   }
-  var dump = [], // dump array
-      array = this; // reasigning this (current array) to the var array
-  for(i = 0; i <= num - 1; i++) { // simple looping
-    dump.push(array[i]); // and pushing
+  var dump = [], 
+      array = this; 
+  for(var i = 0; i <= num - 1; i++) { 
+    dump.push(array[i]); 
   } 
-  return dump; // returning the new array
+  return dump; 
 }
 
 
@@ -81,23 +46,32 @@ Array.prototype.first = function(num) {
 
 
 
-
-
-
-//
-// Array.empty()
-//
-// Returns true if the array is empty, if it's not it'll
-// return the array.
-//
-// @params none
-// @return true, array
-// 
-Array.prototype.empty = function(num) {
-  if(this.length > 0) {
-    return this;
+/*
+ * ## Array.last()
+ *
+ * Simply returns the last element of an array. If a number is given
+ * as a parameter it will return the last N elements.
+ * 
+ * ```js
+ * [0,1,2,3].last() // => 3
+ * [0,1,2,3,4,5,6].last(3) // => [6,5,4]
+ * ```
+ *
+ * @params none, number
+ * @return last element; {array} range
+*/ 
+Array.prototype.last = function(num) {
+  
+ if(typeof num !== 'number') {
+   return this[this.length - 1];
+ } 
+  
+  var dump = [], 
+      array = this; 
+  for(var i = 1; i <= num; i++) {
+    dump.push(array[array.length - i]);
   }
-  return true;
+  return dump; 
 }
 
 
@@ -107,52 +81,18 @@ Array.prototype.empty = function(num) {
 
 
 
-
-//
-// Array.filter()
-//
-// Takes a conditional function and returns every
-// element of the array that's resolved truhty
-//
-// var arr = [0, 15, 6, 13, 20, 'test'];
-// 
-// arr.filter(function(item) {
-//    return item > 10;
-// }); // => [15, 13, 20]
-//
-// @params function
-// @return truthy array elements
-//
-Array.prototype.filter = function(condition) {
-  var dump = []; // dump array to operate with
-  // calling forEach on the array
-  this.forEach(function(index) { 
-    // testing if the conditional function resolves to true
-    if(condition(index)) {
-      // if so, pushing the index into the dump array
-      dump.push(index);
-    }
-  });
-  // and return the dump array
-  return dump;
-}
-
-
-
-
-
-
-
-
-
-//
-// Array.size()
-//
-// Returns the _computable_ array size (this.length - 1)
-//
-// @params none
-// @return array.length - 1
-//
+/*
+ * ## Array.size()
+ *
+ * Returns the array size (this.length - 1) used in for loops.
+ * 
+ * ```js
+ * [0,1,2,3].size() // => 3
+ * ```
+ *
+ * @params none
+ * @return array.length - 1
+*/
 Array.prototype.size = function() {
   return this.length - 1;
 }
@@ -160,116 +100,122 @@ Array.prototype.size = function() {
 
 
 
-
-
-
-
-
-//
-// Array.reverse() - this function actually exists!
-// 
-// Takes an given array and returns a reversed version.
-//
-// @params none
-// @return reversed array
-// 
-Array.prototype.reverse = function() {
-  var dump = [], // a dump array that'll be returned
-      array = this, // reassigning this (current array) to a var
-      i;
-  // now we just go through all the items, pushing them into the array...
-  for(i = 1; i <= array.length; i++) {
-    // startung with the last element and going down to element 0. 
-    dump.push(array[array.length - i]);
-  }
-  return dump; // return the dump array
+/*
+ * ## Array.filter()
+ *
+ * Takes a conditional function and returns every
+ * element of the array that's resolved truhty
+ *
+ * ```js
+ * var arr = [0, 15, 6, 13, 20, 'test'];
+ * 
+ * arr.filter(function(item) {
+ *    return item > 10;
+ * }); // => [15, 13, 20]
+ * ```
+ *
+ * @params function
+ * @return truthy array elements
+*/
+Array.prototype.filter = function(condition) {
+  var dump = [];
+  this.each(function(index) { 
+    if(condition(index)) {
+      
+      dump.push(index);
+    }
+  });
+  return dump;
 }
 
 
 
 
-
-
-
-
-
-//
-// Array.contains()
-//
-// Checks an array for one or more entries, seperated by comma (,)
-// Notice: If one of the searches is not found this function will return
-// false, otherwise it will return the complete array.
-//
-// @params String(s) of search words
-// @return array; self
-//
-// [0,1,2,3,4,5].contains(3,4); // => [0,1,2,3,4,5]
-// [0, 2, 'test'].contains('my test') // => false
-//
-
-Array.prototype.contains = function() {
-  var found = [], // an array of all found elements (this is important!)
-      array = this, // a reference to the current array
-      search, // search - either one or more arguments
-      i;
+/*
+ * ## Array.reverse() 
+ * This function already exists, anyway let's rebuild it!!
+ * 
+ * Takes an given array and returns a reversed version.
+ *
+ * ```js
+ * [1,2,3,4,5].reverse(); // => [5,4,3,2,1]
+ * ```
+ * @params none
+ * @return reversed array
+*/ 
+Array.prototype.reverse = function() {
+  var array = this,
+      dump = this.copy(),
+      j; 
   
-  // if there's more than one argument passed
-  if(arguments.length > 1) {
-    // make search an array
+  for(var i = 0; i <= array.length; i++) {
+     j = i-1;
+     array[j] = dump[dump.length - i];
+  }
+  return array; 
+}
+
+
+
+/*
+ * ## Array.contains()
+ *
+ * Checks an array for one or more entries, separated by comma (,)
+ * *Notice*: If one of the searches is not found this function will return
+ * false, otherwise it will return the complete array.
+ *
+ * ```js
+ * [0,1,2,3,4,5].contains(3,4); // => [0,1,2,3,4,5]
+ * [0, 2, 'test'].contains('my test') // => false
+ * ```
+ *
+ * @params String(s) of search words
+ * @return array; self
+ *
+*/
+Array.prototype.contains = function() {
+  var found = [], 
+      array = this, 
+      search; 
+
+  if(arguments.length > 1) { 
     search = [];
-    // loop through all the arguments (-1; just like normal arrays)
-    for(i = 0; i <= arguments.length - 1; i++) {
-      // call contains() on every single argument
+    for(var i = 0; i <= arguments.length - 1; i++) {
       if(array.contains(arguments[i])) {
-        // and if it returns the array (so not false), push it to
-        // the found elements
         found.push(arguments[i]);
       }
     }
-    // if the found array is as long as the arguments array,
-    // we can be sure everything was found and return true
+
     if(found.length === arguments.length) {
       return true;
     }
-  // if arguments is not bigger than 1
-  } else {
-    // asume we're not searching for multipe things
+  
+  } else {   
     search = arguments[0];
   }
-  
-  // if it's a single-search, just use indexOf() to check if the
-  // index we search is present.
-  // indexOf will return the ARRAY index 
-  // [1,2,3].indexOf(2); // => 1
-  // or -1 if the element is not in the array.
+
   if(this.indexOf(search) !== -1) {
     return this;
   }
-  // if all things fail, return false - the searched index
-  // is not in this array.
+
   return false;
 }
 
 
 
 
-
-
-
-
-
-//
-// Array.copy()
-//
-// Create a copy of the current array.
-//
-// @params none
-// @return new array
-//
+/*
+ * ## Array.copy()
+ *
+ * Create a copy of the current array.
+ *
+ * @params none
+ * @return new array
+ */
 Array.prototype.copy = function() {
   var array = [];
   
-  this.forEach(function(index) {
+  this.each(function(index) {
     array.push(index);
   });
 
@@ -279,76 +225,237 @@ Array.prototype.copy = function() {
 
 
 
-
-
-
-
-
-//
-// Array.replace()
-//
-// Takes a search word (array element) and replaces it with the
-// replacement given as second parameter.
-//
-// @params search, replacement
-// @return replaced element
-// 
+/*
+ * ## Array.replace()
+ *
+ * Takes a search word (array element) and replaces it with the
+ * replacement given as second parameter.
+ *
+ * ```js
+ * [0,1,2,3,4,5].replace(5, 'test'); // => [0,1,2,3,4,'test']
+ * ```
+ *
+ * @params search, replacement
+ * @return array
+ */ 
 Array.prototype.replace = function(search, replacement) {
   var i,
       array = this.copy();
-  // loop through the array
   for(i = 0; i <= array.size(); i++) {
-    // if the current element is what we search
     if(array[i] === search) {
-      // replace it with the replacement
       array[i] = replacement;
-      // return the array
       return array;
     }
   }
-  // search wasn't found; return false
   return false;
 }
 
 
 
 
-
-
-
-
-
-//
-// Array.merge()
-//
-// Merges the given array with the array it operates on into one big array.
-//
-// @params one or more arrays
-// @return merged array
-//
+/*
+ * ## Array.merge()
+ *
+ * @TODO Make array merge() go any level deep! ALL THE WAY DOWN! \o/
+ *
+ * Merges the given array with the array it operates on into one big array.
+ *
+ * ```
+ * ['my', 'array'].merge([4,8,15], [16,23,42]); // => ['my', 'array', 4, 8, 15, 16, 23, 42]
+ * ``` 
+ *
+ * @params one or more arrays
+ * @return merged array
+ */
 Array.prototype.merge = function() {
   var i = 0,
-      j = 0,
-      array = [];
-  // if there are no arguments there's nothing to work on.
+      j = 0;
+  
   if(!arguments) {
     return false;
   }
 
-  // copy the array we work on into the
-  // local "array" variables, this way the
-  // original array is not overwritten
-  var array = this.copy();
-
-  // iterate over all the arguments (which are most likely arrays)
   for(i = 0; i <= arguments.length - 1; i++) {
-    // if they are arrays, iterate over them
     for(j = 0; j <= arguments[i].length - 1; j++) {
-      // and push each array element into the array we work on
-      array.push(arguments[i][j]);
+      this.push(arguments[i][j]);
     }
   }
 
-  // return the new array
+  return this;
+}
+
+
+
+
+/*
+ * ## Array.random()
+ *
+ * Returns a random element from the array, optional within a special range.
+ *
+ * ```
+ * [1,2,3,4].random() // => 2
+ * [1,2,3,4].random() // => 4
+ * [1,2,3,4].random(1,3) // => 3
+ * ```
+ *
+ * @params {Number} - Max number, if one is given
+ * @params {Number} - Max and min number
+ * @params {None}   - Max is the array's size, min is zero
+ *
+ * */
+
+Array.prototype.random = function() {
+  var max,
+      min,
+      a,
+      b,
+      random,
+      size = this.length,
+      args = arguments,
+      variableSwitch = {
+        0: function() {
+          max = size;
+          min = 0;
+        },
+        1: function() {
+          max = args[0];
+          min = 0;
+        },
+        2: function() {
+          a = args[0];
+          b = args[1];
+          if(a > b) {
+            max = a;
+            min = b;
+          } else {
+            max = b;
+            min = a;
+          }
+        }
+      }
+  
+  var assignVariables = variableSwitch[args.length];
+  assignVariables();
+   
+  random = Math.floor( (Math.random() * (max - min)) + min );
+  return this[random];
+}
+
+/**
+ * ## Array.each()
+ *
+ * Performce a callback function on every element of the array.
+ * 
+ * ```
+ *  ['Lorem', 'Ipsum', 'Dolor'].each(function(item) {
+ *    console.log(item);
+ *  }); // => Lorem, Ipsum, Dolor
+ * ```
+ * 
+ * @param {Function} Function to be called on every element
+ * @return {Array} The original array
+ */
+Array.prototype.each = function(cb) {
+  var array = this,
+      i = 0;
+  for(i; i < array.length; i++) {
+    cb(array[i]);
+  }
+  return this;
+}
+
+
+
+/**
+ *
+ * ## Array.prepend()
+ *
+ * Prepands the current array with a new element, taking position 0 in the array.
+ *
+ * ```
+ * ['Array', 'is', 'awesome'].prepand('My');
+ * // => ['My', 'Array', 'is', 'awesome'];
+ */
+
+Array.prototype.prepend = function(prependIndex) {
+  if(!prependIndex) {
+    return this;
+  }
+
+  return this.reverse().append(prependIndex).reverse();
+}
+
+
+/**
+ * ## Array.append()
+ *
+ * Wrapper function for Array.push().
+ *
+ * @param {Any} Anything
+ * @return {Array} Array with new element added to the end
+ *
+ */
+Array.prototype.append = function(append) {
+  if(!append) {
+    return this;
+  }
+
+  this.push(append);
+
+  return this;
+}
+
+
+
+/**
+ * ## Array.concat()
+ *
+ * Combines the existing Array with every thing it is passed. This is like Array.merge() but 
+ * accepts everything as argument.
+ *
+ * @params {Array} - Any number of Array(s) that should be merged into the existing Array
+ */
+
+Array.prototype.concat = function() {
+  var dump = this;
+  
+  // If there's no argument just return the element.
+  if(arguments.length === 0) {
+    return this;
+  } 
+  for(key in arguments) {
+    // Check if the arguments constructor is an Array.
+    if(arguments[key].constructor.toString().indexOf('Array') > -1) {
+      console.log('Array found: ' + arguments[key]);
+      // however, if it's an array call the Array.merge() function on it, passing the
+      // current key as an argument.
+      Array.prototype.merge.call(dump, arguments[key]);
+    } else {
+      // otherwise, just push the argument into the new array.
+      dump.push(arguments[key]);
+    }
+  }
+    // and finally, return the concatinated array!
+    return dump;
+}
+
+/*
+ * ## Array.concatLazy()
+ *
+ * Lazy concatination - everything will be passed as-is without merging or
+ * concatinating.
+ * 
+ * ```
+ * [1,2,3].concatLazy(['My', 'Array'], {some: 'object'}); // => [1,2,3, Array[2], Object];
+ * ```
+ *
+ */
+Array.prototype.concatLazy = function() {
+  var array = this;
+  // just append all arguments, no matter what they are.
+  for(key in arguments) {
+    array.append(arguments[key]);
+  }
+
   return array;
 }
